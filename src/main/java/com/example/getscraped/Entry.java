34dc282e6f901f92a5d53a39af55e9eb815a4ebd
MHWillard <mhwillard@gmail.com>
2022-11-18 15:46:29 -0500

    11.18.22

--- src/main/java/com/example/getscraped/Entry.java
+++ src/main/java/com/example/getscraped/Entry.java
@@ -3,13 +3,14 @@ package com.example.getscraped;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.application.Application;
 
 import java.io.IOException;
 
-public class Application extends javafx.application.Application {
+public class Entry extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Entry.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 400, 250);
         stage.setTitle("Get Scraped Wikipedia Article Scraper");
         stage.setScene(scene);
